e96ae2a858d597ba5af29020664288adf2fe95c1
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-03-31 15:14:44 +0800

    add more tests

--- test/fetcher.test.ts
+++ test/fetcher.test.ts
@@ -1,26 +1,32 @@
 import { describe, expect, vi } from 'vitest'
 import createFetchMock from 'vitest-fetch-mock'
-import { Fetcher } from '../src/index.js'
+import { FetcherError } from '../src/fetcher-error.js'
+import { Fetcher, fetcher } from '../src/fetcher.js'
 
-interface Response {
+interface MockResponse {
   id: number
   userId: number
   title: string
   body: string
 }
 
+const mockResponse = (): MockResponse => ({
+  userId: 1,
+  id: 1,
+  title: 'lorem ipsum',
+  body: 'lorem ipsum'
+})
+
 const fetchMock = createFetchMock(vi)
 fetchMock.enableMocks()
 
-describe('new Fetcher', (test) => {
-  const fetcher = new Fetcher('https://jsonplaceholder.typicode.com')
-
-  const mockResponse = {
-    userId: 1,
-    id: 1,
-    title: 'lorem ipsum',
-    body: 'lorem ipsum'
-  }
+describe('Fetcher', (test) => {
+  const fetcher = new Fetcher('https://jsonplaceholder.typicode.com', {
+    headers: {
+      'X-Test': 'foo',
+      'X-Null': 'bar'
+    }
+  })
 
   test('should be defined', () => {
     expect(fetcher.get).toBeDefined()
@@ -30,13 +36,72 @@ describe('new Fetcher', (test) => {
     expect(fetcher.delete).toBeDefined()
     expect(fetcher.request).toBeDefined()
     expect(fetcher.extends).toBeDefined()
-    expect(fetcher.interceptor).toBeDefined()
   })
 
-  test('mockResponse', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(mockResponse))
-    const response = await fetcher.get<Response>('/posts/1')
-    expect(response.body).toBe(mockResponse.body)
-    expect(fetchMock.mock.calls.length).toBe(1)
+  test('should return a response', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse()))
+    const response = await fetcher.get<MockResponse>('/posts/1')
+    expect(response).toEqual(mockResponse())
+  })
+
+  test('should return a response with path params', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse()))
+    const response = await fetcher.get<MockResponse>('/posts/:postId', {
+      params: { postId: 1 }
+    })
+    expect(response).toEqual(mockResponse())
+  })
+
+  test('extends should return a new fetcher', () => {
+    const newFetcher = fetcher.extends('/posts')
+    expect(newFetcher).toBeInstanceOf(Fetcher)
+    expect(newFetcher).not.toBe(fetcher)
+  })
+
+  test('empty path, used basePath', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse()))
+    const response = await fetcher.get('')
+    expect(response).toEqual(mockResponse())
+  })
+
+  test('combine headers', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse()))
+    const response = await fetcher.get('/posts/1', {
+      headers: {
+        'X-Test': 'bar', // override
+        'X-Test2': 'test'
+      }
+    })
+    expect(response).toEqual(mockResponse())
+  })
+})
+
+describe('fetcher', (test) => {
+  test('should be defined', () => {
+    expect(fetcher).toBeDefined()
+  })
+
+  test('should return a response', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse()))
+    const response = await fetcher<MockResponse>(
+      'https://jsonplaceholder.typicode.com/posts/1'
+    )
+    expect(response).toEqual(mockResponse())
+  })
+
+  test('should return error', async () => {
+    fetchMock.mockResponseOnce('{}', {
+      status: 404,
+      statusText: 'Not Found'
+    })
+
+    try {
+      await fetcher<MockResponse>(
+        'https://jsonplaceholder.typicode.com/posts/bad'
+      )
+    } catch (err) {
+      expect(err).toBeInstanceOf(FetcherError)
+      expect(err.message).toBe('Not Found')
+    }
   })
 })
